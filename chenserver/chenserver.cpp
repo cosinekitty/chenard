e487d0db0136a16cc2a655c5d93b6c3ee7c221bf
Don Cross <cosinekitty@gmail.com>
2015-06-14 19:23:51 -0400

    Started new chenserver project.

---
+++ chenserver/chenserver.cpp
@@ -0,0 +1,31 @@
+/*
+	chenserver.cpp  -  Don Cross  -  http://cosinekitty.com
+
+	A version of the Chenard chess engine designed to be used by
+	software via a very simple command set over a TCP connection.
+*/
+
+#include <cstdio>
+#include <cstdlib>
+#include "chess.h"
+
+int main(int argc, const char *argv[])
+{
+	return 1;
+}
+
+const char *GetWhitePlayerString()  // used by SavePortableGameNotation()
+{
+	return "White";
+}
+
+const char *GetBlackPlayerString() // used by SavePortableGameNotation()
+{
+	return "Black";
+}
+
+void ChessFatal(const char *message)
+{
+	fprintf(stderr, "Fatal chess error: %s\n", message);
+	exit(1);
+}
