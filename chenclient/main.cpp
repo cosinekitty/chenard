ea999c7e8d888717026c175e8ff7fac93fe547a8
Don Cross <cosinekitty@gmail.com>
2015-06-23 20:49:22 -0400

    Fixed chenclient memory leak and eliminated arbitrary size limit for the response.

--- chenclient/main.cpp
+++ chenclient/main.cpp
@@ -1,9 +1,12 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #include <stdlib.h>
+#include <string>
 #include <iostream>
 #include <WinSock2.h>
 #include <WS2tcpip.h>
+
+bool SendCommand(const std::string& server, int port, const std::string& command, std::string& response);
 
 int main(int argc, const char *argv[])
 {
@@ -30,58 +33,10 @@ int main(int argc, const char *argv[])
     {
         if (data.wVersion == versionRequested)
         {
-            hostent *host = gethostbyname(server);
-            if (host == nullptr)
+            std::string response;
+            if (SendCommand(server, port, message, response))
             {
-                std::cerr << "ERROR: Cannot resolve host '" << server << "'" << std::endl;
-            }
-            else
-            {
-                const char *iptext = inet_ntoa(**(in_addr **)host->h_addr_list);
-                std::cout << "Resolved host '" << host->h_name << "' = " << iptext << std::endl;
-                SOCKADDR_IN target;
-                target.sin_family = AF_INET;
-                target.sin_port = htons(port);
-                target.sin_addr.s_addr = inet_addr(iptext);
-
-                SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-                if (sock == INVALID_SOCKET)
-                {
-                    std::cerr << "Cannot open socket to host" << std::endl;
-                }
-                else
-                {
-                    if (0 != connect(sock, (SOCKADDR *)&target, sizeof(target)))
-                    {
-                        std::cerr << "Cannot connect to host" << std::endl;
-                    }
-                    else
-                    {
-                        send(sock, message, strlen(message), 0);
-                        send(sock, "\n", 1, 0);
-
-                        const int BUFFERSIZE = 1024;
-                        char *response = new char[BUFFERSIZE];
-                        int bytesRead = recv(sock, response, BUFFERSIZE, MSG_WAITALL);
-
-                        if (bytesRead <= 0)
-                        {
-                            std::cerr << "ERROR: no response received" << std::endl;
-                        }
-                        else
-                        {
-                            for (int i = 0; i < bytesRead; ++i)
-                            {
-                                std::cout.put(response[i]);
-                            }
-                            std::cout << std::endl;
-                        }
-                    }
-                    closesocket(sock);
-                    sock = INVALID_SOCKET;
-                }
-
-                std::cerr << std::endl;
+                std::cout << response << std::endl;
             }
         }
         else
@@ -95,4 +50,80 @@ int main(int argc, const char *argv[])
         std::cerr << "ERROR: Could not initialize WinSock." << std::endl;
     }
     return 1;
+}
+
+
+bool SendCommand(const std::string& server, int port, const std::string& command, std::string& response)
+{
+    bool success = false;
+    response.clear();
+
+    hostent *host = gethostbyname(server.c_str());
+    if (host == nullptr)
+    {
+        std::cerr << "ERROR: Cannot resolve host '" << server << "'" << std::endl;
+    }
+    else
+    {
+        const std::string iptext = inet_ntoa(**(in_addr **)host->h_addr_list);
+        std::cout << "Resolved host '" << host->h_name << "' = " << iptext << std::endl;
+        SOCKADDR_IN target;
+        target.sin_family = AF_INET;
+        target.sin_port = htons(port);
+        target.sin_addr.s_addr = inet_addr(iptext.c_str());
+
+        SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (sock == INVALID_SOCKET)
+        {
+            std::cerr << "Cannot open socket to host" << std::endl;
+        }
+        else
+        {
+            if (0 != connect(sock, (SOCKADDR *)&target, sizeof(target)))
+            {
+                std::cerr << "Cannot connect to host" << std::endl;
+            }
+            else
+            {
+                send(sock, command.c_str(), command.length(), 0);
+                send(sock, "\n", 1, 0);
+
+                while (true)    // keep looping and reading blocks of memory until we find "\n" or hit an error.
+                {
+                    const int BUFFERSIZE = 128;
+                    char buffer[BUFFERSIZE];
+                    int bytesRead = recv(sock, buffer, BUFFERSIZE, 0);
+                    if (bytesRead < 0)
+                    {
+                        std::cerr << "ERROR: Problem reading response from socket." << std::endl;
+                        break;
+                    }
+
+                    if (bytesRead == 0)
+                    {
+                        std::cerr << "ERROR: Incomplete response." << std::endl;
+                        break;
+                    }
+
+                    for (int i = 0; i < bytesRead; ++i)
+                    { 
+                        if (buffer[i] == '\n')
+                        {
+                            // We found the end of the message.
+                            success = true;
+                            goto end_of_message;    // break out of both loops
+                        }
+                        response.push_back(buffer[i]);
+                    }
+                }
+            }
+end_of_message:
+            closesocket(sock);
+        }
+    }
+    if (!success)
+    {
+        response.clear();
+    }
+    return success;
 }
